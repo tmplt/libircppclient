62541d59a34332d9cee14fe3c83e1aced91e7f08
Tmplt <ttemplate223@gmail.com>
2016-01-09 17:54:14 +0100

    More blind and more questions
    - Make it clear what Boost version is downloaded
    - For some reason exports in external scripts aren't saved. I wonder why.

--- util/travis/install_boost.sh
+++ util/travis/install_boost.sh
@@ -3,7 +3,12 @@
 sudo apt-get install build-essential
 mkdir -p libs/boost && cd libs/boost
 
-echo "Downloading Boost-$(echo $BOOST_VERSION | sed 's/_/\./g')"
+echo "
+           ------------------------
+           Downloading Boost-$(echo $BOOST_VERSION | sed 's/_/\./g')
+           ------------------------
+
+"
 wget "http://downloads.sourceforge.net/project/boost/boost/1.60.0/boost_$BOOST_VERSION.tar.bz2" -O boost.tar.bz2
 tar xjf boost.tar.bz2
 cd ./boost*
