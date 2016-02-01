e144def4dec2ec2dd1c22790abec46b1cd4411ba
Tmplt <ttemplate223@gmail.com>
2016-02-01 16:42:09 +0100

    Additional IRC commands
    and those to come in the future.

--- src/commands.cpp
+++ src/commands.cpp
@@ -49,5 +49,55 @@ void client::quit(const std::string &message)
     stop();
 }
 
+void client::away(const std::string &message)
+{
+    if (message.empty())
+        con.write("AWAY");
+    else
+        con.write("AWAY " + message);
+}
+
+void client::part(const std::string &chans)
+{
+    con.write("PART " + chans);
+}
+
+void client::topic(const std::string &chan, const std::string &topic)
+{
+    if (topic.empty())
+        con.write("TOPIC " + chan);
+    else
+        con.write("TOPIC " + chan + " :" + topic);
+}
+
+void client::names(const std::string &chans)
+{
+    if (chans.empty())
+        con.write("NAMES");
+    else
+        con.write("NAMES " + chans);
+}
+
+void client::invite(const std::string &nick, const std::string &chan)
+{
+    con.write("INVITE " + nick + ' ' + chan);
+}
+
+void client::kick(const std::string &chans, const std::string &nicks, const std::string &comment)
+{
+    if (comment.empty())
+        con.write("KICK " + chans + ' ' + nicks);
+    else
+        con.write("KICK " + chans + ' ' + nicks + " :" + comment);
+}
+
+void client::list(const std::string &chans)
+{
+    if (chans.empty())
+        con.write("LIST");
+    else
+        con.write("LIST " + chans);
+}
+
 /* ns irc */
 }
--- src/libircppclient.hpp
+++ src/libircppclient.hpp
@@ -32,18 +32,6 @@ class client {
 public:
     client(const config &c);
 
-    void nick(const std::string &nick);
-    void join(const std::string &chan, const std::string &key = "");
-    void msg(const std::string &target, const std::string &message);
-    void quit(const std::string &message = "");
-
-    /*
-     * As this lib is not feature-done, yet:
-     * Send the given content directly to the server
-     * as a command.
-     */
-    void raw_cmd(const std::string &content);
-
     void start();
     void stop()
     {
@@ -57,6 +45,123 @@ public:
 
     /* Add a read_handler that may not exist in this class. */
     void add_read_handler(read_handler_t func);
+
+    /*
+     * In all commands where a channel name must/may be passed,
+     * the channel prefix [#|&|...] must be included.
+     */
+
+// Connection registration
+
+    /* Set nickname */
+    void nick(const std::string &nick);
+
+    // Password message
+
+    // User message
+
+    // Server message
+
+    // Operator message
+
+    /*
+     * Disconnect from the server with an optional quit message.
+     * Also stops the irc::client.
+     */
+    void quit(const std::string &message = "");
+
+    // Server quit message
+
+// Channel operations
+
+    /* Join channel(s) */
+    void join(const std::string &chans, const std::string &keys = "");
+
+    /* Leave channel(s) */
+    void part(const std::string &chans);
+
+    //void mode()
+
+    /* Recieve topic, or set it, if a topic is passed. */
+    void topic(const std::string &chan, const std::string &topic = "");
+
+    /*
+     * Recieve a list of nicknames on a channel, or a full list of
+     * all channels and viewable nicknames, if one or more
+     * channels aren't passed.
+     */
+    void names(const std::string &chans = "");
+
+    /*
+     * List all channels and their topics, or only list the topic
+     * for the passed channel(s).
+     */
+    void list(const std::string &chans = "");
+
+    /* Invite a nickname to a channel. */
+    void invite(const std::string &nick, const std::string &chan);
+
+    /* Kick nickname(s) from channel(s) with an optional comment. */
+    void kick(const std::string &channel, const std::string &nick,
+              const std::string &comment = "");
+
+// Server queries and commands
+
+    /* Query the version of the server program. */
+    void version(const std::string &server = "");
+
+    // Stats message
+
+    // Links message
+
+    // Time message
+
+    // Connect message
+
+    // Trace message
+
+    // Admin message
+
+    // Info message
+
+// Sending messages
+
+    /* Message a nick or a channel. */
+    void msg(const std::string &target, const std::string &message);
+
+    /* Notice a channel/nick with a message */
+    void notice(const std::string &nick, const std::string &message);
+
+// User-based queries
+
+    // Who query
+
+    // Whois query
+
+    // Whowas query
+
+// Miscellaneous messages
+
+    // Kill message
+
+    // Error message
+
+// Optional messages
+
+    /*
+     * Unmark yourself as being away, or mark yourself as such,
+     * if a 'message' is passed.
+     */
+    void away(const std::string &message = "");
+
+// End of IRC commands.
+
+    /*
+     * As this lib is not feature-complete yet:
+     * Send the given content directly to the server
+     * as a command.
+     */
+    void raw_cmd(const std::string &content);
 
 protected:
     config conf_;
