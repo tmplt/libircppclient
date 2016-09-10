4b82059be8b26daea8f684a81f5186644dc9b645
Tmplt <ttemplate223@gmail.com>
2016-09-10 14:29:38 +0200

    revert back to spaces
    fits my vim setup better

--- src/libircppclient.cpp
+++ src/libircppclient.cpp
@@ -9,94 +9,94 @@
 namespace irc {
 
 client::client(const config &conf)
-	: conf_(conf), con_(conf_.ssl)
+    : conf_(conf), con_(conf_.ssl)
 {
-	std::string ret = validate_conf(conf);
+    std::string ret = validate_conf(conf);
 
-	if (!ret.empty())
-		throw std::invalid_argument("Configuration error: " + ret);
+    if (!ret.empty())
+        throw std::invalid_argument("Configuration error: " + ret);
 
-	con_.set_addr(conf.address);
-	con_.set_port(conf.port);
+    con_.set_addr(conf.address);
+    con_.set_port(conf.port);
 
-	/*
-	 * "Bind" the external read handlers from connection to
-	 * read_handler() here. Otherwise, you'd need to fiddle
-	 * with connection.hpp inclusion.
-	 */
-	con_.set_ext_read_handler([this](const std::experimental::string_view &content) {
-		this->read_handler(content);
-	});
+    /*
+     * "Bind" the external read handlers from connection to
+     * read_handler() here. Otherwise, you'd need to fiddle
+     * with connection.hpp inclusion.
+     */
+    con_.set_ext_read_handler([this](const std::experimental::string_view &content) {
+        this->read_handler(content);
+    });
 }
 
 std::string client::validate_conf(const config &c)
 {
-	/* Address checking. */
-	if (c.address.empty()) {
-		return "the address is empty.";
-	} else {
-		std::string ret = util::valid_addr(c.address);
+    /* Address checking. */
+    if (c.address.empty()) {
+        return "the address is empty.";
+    } else {
+        std::string ret = util::valid_addr(c.address);
 
-		if (!ret.empty())
-			return "invalid address, reason: " + ret;
-	}
+        if (!ret.empty())
+            return "invalid address, reason: " + ret;
+    }
 
-	/* Port checking. */
-	if (c.port.empty())
-		return "port is empty.";
+    /* Port checking. */
+    if (c.port.empty())
+        return "port is empty.";
 
-	else if (!util::is_integer(c.port))
-		return "port contains one of more non-integers.";
+    else if (!util::is_integer(c.port))
+        return "port contains one of more non-integers.";
 
-	return "";
+    return "";
 }
 
 void client::initialize()
 {
-	con_.write("NICK " + conf_.nick);
-	con_.write("USER " + conf_.user + " 0 * :" + conf_.user);
+    con_.write("NICK " + conf_.nick);
+    con_.write("USER " + conf_.user + " 0 * :" + conf_.user);
 }
 
 void client::start()
 {
-	con_.connect();
+    con_.connect();
 
-	/*
-	 * These writes will be put in the io_service's
-	 * queue until a complete connection has been made.
-	 */
-	initialize();
-	con_.run();
+    /*
+     * These writes will be put in the io_service's
+     * queue until a complete connection has been made.
+     */
+    initialize();
+    con_.run();
 }
 
 void client::stop()
 {
-	/*
-	 * Haven't I forgotten something?
-	 * Some async function that needs terminating, maybe?
-	 */
-	con_.stop_ping();
-	con_.stop();
+    /*
+     * Haven't I forgotten something?
+     * Some async function that needs terminating, maybe?
+     */
+    con_.stop_ping();
+    con_.stop();
 }
 
 void client::read_handler(const std::experimental::string_view &content)
 {
-	for (read_handler_t &func: read_handlers_) {
-		func(content);
-	}
+    for (read_handler_t &func: read_handlers_) {
+        func(content);
+    }
 }
 
 void client::add_read_handler(std::function<void(const std::experimental::string_view &)> func)
 {
-	read_handlers_.emplace_back(func);
+    read_handlers_.emplace_back(func);
 }
 
 void client::raw_cmd(const std::experimental::string_view &content)
 {
-	if (content.empty())
-		throw std::invalid_argument("content is empty.");
+    if (content.empty())
+        throw std::invalid_argument("content is empty.");
 
-	con_.write(content.data());
+    con_.write(content.data());
 }
 
 /* ns irc */
